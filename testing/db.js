9e6d39cd6388d40747a9d21c2a8a660015e20984
Omar Mujahid <omar_mjhd@yahoo.com>
2015-10-17 01:30:59 -0400

    Entities made in db.js

--- testing/db.js
+++ testing/db.js
@@ -1,13 +1,49 @@
 var mongoose = require( 'mongoose' );
 var Schema   = mongoose.Schema;
 
-var Post = new Schema({  
+var Post = new Schema({
     user: { type: String, required: true },  
     content: { type: String, required: true},  
+    points: { type: Number, unique: false },
+    comments: [ObjectId]
+});
+
+var Comment = new Schema({
+    user: { type: String, required: true},
+    content: { type: String, required: true},
     points: { type: Number, unique: false }
+})
+
+var DoctorClinic = new Schema({
+    ClinicID: { type: ObjectId, required: true},
+    DoctorID: { type: ObjectId, required: true}
+
+});
+
+var Clinic = new Schema({
+    addr: { type: String, required: true},
+    specs: { type: String, required: true}
+});
+
+var Doctor = new Schema({
+    name: { type: String, required: true},
+    licenseNumber: { type: Number, required: true},
+    admin: { type: Boolean, required: true}
+});
+
+var User = new Schema({
+    name: { type: String, required: true},
+    location: { type: String, required: true},
+    gender: {type: String, required: true},
+    admin: { type: Boolean, required: true}
 });
 
 
-mongoose.model('Post', Post);  
+mongoose.model('Post', Post);
+mongoose.model('Comment', Comment);
+mongoose.model('DoctorClinic', DoctorClinic);
+mongoose.model('Clinic', Clinic);
+mongoose.model('Doctor', Doctor);
+mongoose.model('User', User);
 mongoose.connect('mongodb://localhost/cfg');
 
